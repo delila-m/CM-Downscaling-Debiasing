ca547c64854679e4fda1f881216e442825e4a15a
delila-m <128874589+delila-m@users.noreply.github.com>
2026-04-10 14:19:25 -0700

    Finished Bilinear interpolation

--- Functions_Development/temp_downscaling.py
+++ Functions_Development/temp_downscaling.py
@@ -14,38 +14,6 @@ import matplotlib.pyplot as plt
 # Load the iTRACE
 data_xarray = xr.open_dataset("C:/Users/delil/Desktop/NAU/Research/CM Research 2026/CM-Downscaling-Debiasing/Data/atm-surface/itrace.11Ka-0Ka.atm.TREFHT.nc")
 
-# brute force test if xarray's interpolation function works the same...
-test = data_xarray.TREFHT_ANN.interp(lon=[355], lat =[-23.684210526315795], method = "linear")
-test[0] #292.22451782
-
-# simplified manual interpolation 
-control = data_xarray.isel(lon = slice(141, 143), lat = slice(93, 95)).sel(time = "0031-02-15")
-
-range = [0,1]
-
-for i in range:
-    for j in range:
-        print(f"lat: {control.lat.values[i]}")
-        print(f"lon: {control.lon.values[j]}")
-        print(f"value {control.TREFHT_ANN.isel(lat = [i], lon = [j]).values}\n")
-        
-x = 355
-y = -23.684210526315795        
-x1 = 352.5
-x2 = 355.0
-y1 = -23.684210526315795
-y2 = -21.789473684210535
-q11 = 292.56185913
-q12 = 292.72225952
-q21 = 292.22451782
-q22 = 292.41482544
-
-r2 = q12*(x2-x)/(x2-x1) + q22*(x-x1)/(x2-x1)
-r1 = q11*(x2-x)/(x2-x1) + q21*(x-x1)/(x2-x1)
-
-p = r1*(y2-y)/(y2-y1) + r2*(y-y1)/(y2-y1)
-p #292.22451782
-
 # bilinear interpolation function arguments:
     # coordinates for point to be interpolated
     # climate model data as an xarray object
@@ -56,20 +24,24 @@ def bilinear_interpolation( x, y, cm_xarray ):
     q_coords = find_q_coords(x, y, cm_xarray)
             
     # grab data values for Q11, Q12, Q21, Q22 using given coordinates 
+    q11 = cm_xarray.TREFHT_ANN.sel(lon = q_coords['x1'], lat = q_coords['y1'], method = 'nearest') 
+    q12 = cm_xarray.TREFHT_ANN.sel(lon = q_coords['x1'], lat = q_coords['y2'], method = 'nearest') 
+    q21 = cm_xarray.TREFHT_ANN.sel(lon = q_coords['x2'], lat = q_coords['y1'], method = 'nearest') 
+    q22 = cm_xarray.TREFHT_ANN.sel(lon = q_coords['x2'], lat = q_coords['y2'], method = 'nearest') 
     
-    
-    # calculate R1 & R1
+    # calculate R1 & R2
+    r1 = q11*(q_coords['x2']-x)/(q_coords['x2']-q_coords['x1']) + q21*(x-q_coords['x1'])/(q_coords['x2']-q_coords['x1'])
+    r2 = q12*(q_coords['x2']-x)/(q_coords['x2']-q_coords['x1']) + q22*(x-q_coords['x1'])/(q_coords['x2']-q_coords['x1'])
+
     
     # calculate and return P (interpolated point)
+    p = r1*(q_coords['y2']-y)/(q_coords['y2']-q_coords['y1']) + r2*(y-q_coords['y1'])/(q_coords['y2']-q_coords['y1'])
+    return p
 
 
 # internal function to find the coordinates of the q points,
     # or the four closest points to the interpolated point 
 def find_q_coords( x, y, cm_xarray ):   
-    
-    # grab length of lat & long for future use 
-    lat_len = len(cm_xarray.lat)
-    lon_len = len(cm_xarray.lon)
     
     # check if point is inside range 
     if not cm_xarray.lon.values[0] <= x <= cm_xarray.lon.values[lon_len-1]:
@@ -83,46 +55,32 @@ def find_q_coords( x, y, cm_xarray ):
     bi_lon_index = bi.bisect(cm_xarray.lon, x) 
     
     # check to see if the index value is outside longitude range
-    if bi_lon_index == lon_len:  
+    if bi_lon_index == len(cm_xarray.lon) 
         # if outside of range, subtract 1 from index value
         bi_lon_index -= 1   
     
     # locate latitde insertion point using same method 
     bi_lat_index = bi.bisect(cm_xarray.lat, y)
     
-    if bi_lat_index == lat_len:
+    if bi_lat_index == len(cm_xarray.lat):
         bi_lat_index -= 1
     
     # return x1, x2, y1, y2
     return {'x1': cm_xarray.lon.values[bi_lon_index - 1],
             'x2': cm_xarray.lon.values[bi_lon_index],
             'y1': cm_xarray.lat.values[bi_lat_index - 1],
-            'y2':  cm_xarray.lat.values[bi_lat_index]}
+            'y2': cm_xarray.lat.values[bi_lat_index]}
 
+# testing functions 
 q_test = find_q_coords(1, 85, data_xarray)
 q_test        
-    
-    
-    
-    
-    
 
+# seeing if the built in function works the same 
+test_xarray = data_xarray.isel(time = slice(0,4))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+test_interp = bilinear_interpolation(1, 85, test_xarray) 
+test_interp
+test_interp_function = test_xarray.TREFHT_ANN.interp(lon=[1], lat =[85], method = "linear")
+test_interp_function
 
 
